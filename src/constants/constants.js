4aeb20e015ceb45626dbe8039198f4361e91c447
mieum12 <qpdlqltb1215@gmail.com>
2023-11-24 12:40:40 +0900

    feat: 상수 정리

---
+++ src/constants/constants.js
@@ -0,0 +1,25 @@
+export const MESSAGE = {
+  start: "다리 건너기 게임을 시작합니다.",
+  bridge_size: "\n다리의 길이를 입력해주세요.\n",
+  moving: "\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
+  game_command:
+    "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n",
+  result_map: "\n최종 게임 결과",
+  result_state: "\n게임 성공 여부:",
+  total_trial: "총 시도한 횟수:",
+};
+
+export const ERROR = {};
+
+export const GAME = {
+  divider: " | ",
+  move_success: "O",
+  move_fail: "X",
+  move_blank: " ",
+  up: "U",
+  down: "D",
+  retry: "R",
+  quit: "Q",
+  success: '성공',
+  fail: '실패',
+};
